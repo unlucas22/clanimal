57098d9d77c1ef5b77c374d538dd8839309bd603
unlucas22 <unlucas22@gmail.com>
2023-10-30 14:24:51 -0300

    Botones por linea en la tabla

--- resources/views/components/actions/user.blade.php
+++ resources/views/components/actions/user.blade.php
@@ -1,9 +1,9 @@
 @if(!$item->isActive())
-<a wire:click="updateVerifiedAt({{$item->id}})" type="button" class="inline-block py-1 px-2 bg-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out mb-4">Verificar Correo</a>
+<div><a wire:click="updateVerifiedAt({{$item->id}})" type="button" class="inline-block py-1 px-2 bg-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out mb-4">Verificar Correo</a></div>
 @endif
 
-<a wire:click='$emit("openModal", "modal.update.user", @json(["item_id" => $item->id]))' type="button" class="inline-block py-1 px-2 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out mb-4" >Editar</a>
+<div><a wire:click='$emit("openModal", "modal.update.user", @json(["item_id" => $item->id]))' type="button" class="inline-block py-1 px-2 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out mb-4" >Editar</a></div>
 
 @if($item->histories_count)
-<a wire:click='$emit("openModal", "modal.history", @json(["item_id" => $item->id]))' type="button" class="inline-block py-1 px-2 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out mb-4" >Historial</a>
+<div><a wire:click='$emit("openModal", "modal.history", @json(["item_id" => $item->id]))' type="button" class="inline-block py-1 px-2 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out mb-4" >Historial</a></div>
 @endif
