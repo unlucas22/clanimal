2342cff502b492f211cd866985286cddfc9ce0c9
unlucas22 <unlucas22@gmail.com>
2024-03-10 09:20:23 -0300

    cambios varios

--- resources/views/livewire/dashboard/sales.blade.php
+++ resources/views/livewire/dashboard/sales.blade.php
@@ -87,7 +87,7 @@
 
                                         <div class="flex justify-between gap-2">
                                             <div>
-                                                <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Procesar Pago</button>
+                                                <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Procesar Pago (FALTA ESTO)</button>
                                             </div>
                                         </div>
                                     </td>
