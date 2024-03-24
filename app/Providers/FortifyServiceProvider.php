d73f77e0bbbcdcf92dd8c7b840b4f4c88a418ff1
unlucas22 <unlucas22@gmail.com>
2024-03-24 14:18:35 -0300

    ReCaptcha

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -13,6 +13,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\{Session, Auth};
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -46,17 +49,64 @@ class FortifyServiceProvider extends ServiceProvider
 
         Fortify::authenticateUsing(function (Request $request) {
 
-            $response = Http::post('https://www.google.com/recaptcha/api/siteverify?secret=' . config('app.captcha_secret_key') . '&response=' . $request['g-recaptcha-response']);
-            
-            $captcha = $response->json();
-            
-            ddd($captcha);
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+                'g-recaptcha-response' => 'required|captcha',
+            ], [
+                'email.required' => 'El campo correo electrónico es obligatorio.',
+                'email.email' => 'El correo electrónico debe ser una dirección de correo electrónico válida.',
+                'password.required' => 'El campo contraseña es obligatorio.',
+                'g-recaptcha-response.required' => 'Por favor, completa el ReCaptcha.',
+                'g-recaptcha-response.captcha' => 'El ReCaptcha es inválido. Por favor, inténtalo de nuevo.',
+            ]);
 
-            if (!$captcha > .3) {
-                return $user;
-            } else {
-                return session()->flash('error', 'Google thinks you are a bot, please refresh and try again');
+            $credentials = $request->only('email', 'password');
+
+            if (! auth()->attempt($credentials))
+            {
+                return false;
             }
+
+            return Auth::user();
+
+            /*
+            $data = [
+                'secret' => config('app.captcha_secret_key'),
+                'response' => $request['g-recaptcha-response'],
+            ];
+
+            $ch = curl_init();
+
+            // Configura las opciones de la solicitud
+            curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // Realiza la solicitud y obtén la respuesta
+            $response = curl_exec($ch);
+
+            // Verifica si la solicitud fue exitosa y si la respuesta es válida
+            if ($response !== false)
+            {
+                $data = json_decode($response, true);
+
+                ddd($data);
+
+                // El ReCaptcha es válido, continuar con el proceso de autenticación o lo que sea necesario
+                if ($data['success'])
+                {
+                    return $user;
+                }
+                else
+                {
+                    return session()->flash('error', 'Google thinks you are a bot, please refresh and try again');
+                }
+            } else {
+                ddd($ch);
+            }
+            */
         });
     }
 }
